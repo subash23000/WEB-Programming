49a8bfe257f0dd2b1f17509de95a8c6160180c0a
Subash Chhetri <subash23000@student.hamk.fi>
2025-02-03 10:38:31 +0200

    Add files via upload

---
+++ webprogramming25/src/index.php
@@ -0,0 +1,5 @@
+<?php
+http://localhost:81/
+echo "<h1>hello world this is test</h1>";
+phpinfo();
+?>
---
+++ webprogramming25/src/web.php
@@ -0,0 +1,16 @@
+<?php 
+
+echo "Hello";
+
+$var1 = "World<br/>";
+echo $var1 . "<br/>";
+echo "Hello" .$var1;
+
+
+
+$string1 = "php somethng";
+echo $string1;
+
+$var2 = "Hello";
+
+?>
